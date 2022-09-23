57405b3003989b238ee0f9b124add05293c127dc
Keri Nguyen <k4zy007@outlook.com>
2022-09-23 09:17:24 +0700

    Fix issues

--- add_dot_int.py
+++ add_dot_int.py
@@ -1,5 +1,6 @@
 def reformat_text(text):
     # add "." every 3 digits
+    text = str(text)
     text = text[::-1]
     text = ".".join(text[i:i + 3] for i in range(0, len(text), 3))
     text = text[::-1]

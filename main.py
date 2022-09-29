7b6cea349acd8f2332a1367728777ea76ade803e
Keri Nguyen <k4zy007@outlook.com>
2022-09-29 12:50:34 +0700

    Fix main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from read_file_output import read_file_output
 import datetime
 import time
 import os
+import json
 
 
 def set_up_terminal():
@@ -16,42 +17,35 @@ def set_up_terminal():
 
 
 def main():
+    list_export_to_json = []
+    dic_in_json = {"thang" : 0, "thu_nhap_thang": 0, "so_nguoi_phu_thuoc": 0, "tien_bao_hiem": 0}
     set_up_terminal()
     print()
     print("Chương trình tính thuế thu nhập cá nhân")
     print()
-    print("Chọn cách nhập dữ liệu: ")
-    print("1. Nhập dữ liệu từ file")
-    print("2. Nhập dữ liệu từ bàn phím")
-    print("3. Đọc kết quả đã tính thuế")
-    print("4. Thoát")
+    print("1. Nhập dữ liệu thu nhập tháng")
+    print("2. Đọc kết quả đã tính thuế")
+    print("3. Thoát")
 
     print()
     choice = input("Nhập lựa chọn của bạn: ")
 
     while True:
+        # if choice == "1":
+        #     os.system("cls")
+        #     print("Chương trình tính thuế thu nhập cá nhân - Đọc dữ liệu từ file")
+        #     print()
+        #     ten_nguoi_tinh_thue = input("Nhập tên của bạn: ")
+        #
+        #     cal_from_file(ten_nguoi_tinh_thue)
+        #     # trở lại menu chọn chức năng
+        #     main()
+
         if choice == "1":
-            os.system("cls")
-            print("Chương trình tính thuế thu nhập cá nhân - Đọc dữ liệu từ file")
-            print()
-            ten_nguoi_tinh_thue = input("Nhập tên của bạn: ")
-
-            cal_from_file(ten_nguoi_tinh_thue)
-            # trở lại menu chọn chức năng
-            main()
-
-        elif choice == "2":
             os.system("cls")
             print("Chương trình tính thuế thu nhập cá nhân nhập dữ liệu từ bàn phím")
             print()
             ten_nguoi_tinh_thue = input("Nhập tên của bạn: ")
-            while True:
-                try:
-                    so_thue_tam_nop = int(input("Nhập số tiền thuế bạn đã nộp tạm (triệu VNĐ): "))
-                    break
-                except ValueError:
-                    print("Bạn phải nhập số nguyên dương. Vui lòng nhập lại!")
-                    continue
             ten_nguoi_tinh_thue_folder = convert(ten_nguoi_tinh_thue.lower().replace(" ", "_"))
             # tạo thư mục output nếu chưa có
             if not os.path.exists("output"):
@@ -63,10 +57,10 @@ def main():
             # Tạo đối tượng tính thuế
             tinh_thue = TinhThue()
             tinh_thue.nhap_nam()
-            tien_thue_ca_nam = 0
+            tien_thue_ca_nam_tam_nop = 0
             for nam in tinh_thue.nam:
                 table = PrettyTable(title=f"Bảng tính thuế thu nhập cá nhân năm {nam} của {ten_nguoi_tinh_thue}")
-                table.field_names = ["Tháng", "Thu nhập cá nhân (VND)", "Thu nhập chịu thuế (VND)", "Tiền Thuế (VND)"]
+                table.field_names = ["Tháng", "Thu nhập cá nhân (VND)", "Thu nhập chịu thuế (VND)", "Thuế tạm nộp (VND)"]
                 while True:
                     try:
                         phu_thuoc = int(input(f"Nhập số người phụ thuộc năm {nam}: "))
@@ -76,7 +70,7 @@ def main():
                         continue
                 while True:
                     try:
-                        bao_hiem_xa_hoi = int(input(f"Nhập số tiền bảo hiểm xã hội năm {nam} (triệu VNĐ): "))
+                        bao_hiem_xa_hoi = int(input(f"Nhập số tiền bảo hiểm xã hội hàng tháng (triệu VNĐ): "))
                         break
                     except ValueError:
                         print("Bạn phải nhập số nguyên dương. Vui lòng nhập lại!")
@@ -87,8 +81,7 @@ def main():
                 for thang in tinh_thue.thang:
                     tinh_thue.nhap_thong_tin(thang, phu_thuoc, bao_hiem_xa_hoi)
                     tinh_thue.tinh_thue()
-                    tien_thue_hang_thang = {"Thang": thang, "Tien thue": tinh_thue.tien_thue_thang}
-                    tinh_thue.thong_ke_tien_thue_cac_nam.append(tien_thue_hang_thang)
+
                     print()
                     print(f"Tiền thuế tháng {thang}: {tinh_thue.tien_thue_thang} VND")
                     print()
@@ -97,24 +90,25 @@ def main():
                         [thang, reformat_text(str(tinh_thue.thu_nhap)),
                          reformat_text(str(tinh_thue.thu_nhap_chiu_thue)),
                          reformat_text(str(tinh_thue.tien_thue_thang))])
-                    tien_thue_ca_nam += tinh_thue.tien_thue_thang
+                    tien_thue_ca_nam_tam_nop += tinh_thue.tien_thue_thang
+                    # lưu thông tin vào list_export_to_json
+
                     if thang == 12:
                         now = datetime.datetime.now()
-                        table.add_row(["", "", f"Tổng thuế TNCN cả năm {nam}", reformat_text(str(tien_thue_ca_nam))])
+                        table.add_row(["", "", f"Tổng tạm thu", reformat_text(str(tien_thue_ca_nam_tam_nop))])
                         # cột Tháng căn giữa
                         table.align["Tháng"] = "c"
+                        tinh_thue.tinh_thue_nam()
+                        table_year = PrettyTable()
+                        table_year.field_names = ["Thu nhập cả năm", "Thu nhập chịu thuế cả năm", "Thuế TNCN thực thu",
+                                                  "Tiền được nhận lại"]
+                        table_year.add_row([reformat_text(str(tinh_thue.thu_nhap_nam)),reformat_text(str(tinh_thue.thu_nhap_chiu_thue_nam)), reformat_text(str(tinh_thue.tien_thue_thuc_te)), reformat_text(str(tien_thue_ca_nam_tam_nop - tinh_thue.tien_thue_thuc_te))])
+
                         with open(f"output/{ten_nguoi_tinh_thue_folder}/{ten_nguoi_tinh_thue_folder}_tienThueNam{nam}.txt",
                                   "w") as f:
                             f.write(str(table))
                             f.write("\n")
-                            f.write(f"Thuế tạm nộp: {so_thue_tam_nop} triệu VND\n")
-                            if (tien_thue_ca_nam - so_thue_tam_nop * 1000000) > 0:
-                                f.write(
-                                    f"Thuế TNCN còn phải nộp: {reformat_text(str(tien_thue_ca_nam - int(so_thue_tam_nop) * 1000000))} VND\n")
-                            else:
-                                f.write(
-                                    f"Tiền được nhận lại: {reformat_text(str(int(so_thue_tam_nop) * 1000000 - tien_thue_ca_nam))} VND\n")
-                            f.write("\n")
+                            f.write(str(table_year) + "\n")
                             f.write(f"Ngày tạo file: {now.strftime('%d/%m/%Y %H:%M:%S')}")
                             f.close()
                         # Lưu bảng vào file có sẵn tienThueCacNam.txt
@@ -123,26 +117,12 @@ def main():
                                 "a") as f:
                             f.write(str(table))
                             f.write("\n")
-                            f.write(f"Thuế tạm nộp: {so_thue_tam_nop} triệu VND\n")
-                            if (tien_thue_ca_nam - so_thue_tam_nop * 1000000) > 0:
-                                f.write(
-                                    f"Thuế TNCN còn phải nộp: {reformat_text(str(tien_thue_ca_nam - int(so_thue_tam_nop) * 1000000))} VND\n")
-                            else:
-                                f.write(
-                                    f"Tiền được nhận lại: {reformat_text(str(int(so_thue_tam_nop) * 1000000 - tien_thue_ca_nam))} VND\n")
-                            f.write("\n")
+                            f.write(str(table_year) + "\n")
                             f.write(f"Ngày tạo file: {now.strftime('%d/%m/%Y %H:%M:%S')}")
                             f.write("\n\n")
                             f.close()
                         print(f"{table}\n")
-                        print(
-                            f"{ten_nguoi_tinh_thue} đã nộp tạm {reformat_text(so_thue_tam_nop * 1000000)}  VND thuế TNCN năm {nam}" + "\n")
-                        if (tien_thue_ca_nam - int(so_thue_tam_nop) * 1000000) > 0:
-                            print(
-                                f"Thuế TNCN còn phải nộp: {reformat_text(str(tien_thue_ca_nam - int(so_thue_tam_nop) * 1000000))} VND" + "\n")
-                        else:
-                            print(
-                                f"Tiền được nhận lại: {reformat_text(str(int(so_thue_tam_nop) * 1000000 - tien_thue_ca_nam))} VND" + "\n")
+                        print(f"{table_year}\n")
                         print(f"Ngày tạo file: {now.strftime('%d/%m/%Y %H:%M:%S')}")
                         print(f"Đã lưu file output vào thư mục output/{ten_nguoi_tinh_thue_folder}\n")
             continue_cal = input("Bạn có muốn tiếp tục tính thuế không? (Y/N): ").lower()
@@ -151,14 +131,14 @@ def main():
             else:
                 main()
 
-        elif choice == "3":
+        elif choice == "2":
             # Đọc file output
             os.system("cls")
             print("Đọc file output")
             name_input = convert(input("Nhập tên người bạn muốn xem: ").lower().replace(" ", "_"))
             read_file_output(name_input)
             main()
-        elif choice == "4":
+        elif choice == "3":
             print("Cảm ơn bạn đã sử dụng chương trình!")
             # delay 3s rồi thoát
             time.sleep(3)
--- tinhthue.py
+++ tinhthue.py
@@ -1,14 +1,17 @@
 class TinhThue:
-
-    mien_tru = 11000000
+    mien_tru_thang = 11000000
+    mien_tru_nam = 132000000
     so_nguoi_phu_thuoc = 0
     tien_bao_hiem = 0
-    thong_ke_tien_thue_cac_nam = []
+    # thong_ke_tien_thue_cac_nam = []
+    thu_nhap_nam = 0
     thu_nhap = 0
     thu_nhap_chiu_thue = 0
     tien_thue_thang = 0
     thang = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
     nam = []
+    thu_nhap_chiu_thue_nam = 0
+    tien_thue_thuc_te = 0
 
     def __init__(self):
         pass
@@ -25,14 +28,16 @@ class TinhThue:
 
     def nhap_thong_tin_file(self, thu_nhap):
         self.thu_nhap = int(thu_nhap) * 1000000
-        self.thu_nhap_chiu_thue = self.thu_nhap - self.tien_bao_hiem - self.mien_tru - self.so_nguoi_phu_thuoc * 4400000
-
+        self.thu_nhap_chiu_thue = self.thu_nhap - self.tien_bao_hiem - self.mien_tru_thang - self.so_nguoi_phu_thuoc * 4400000
+        self.thu_nhap_nam += self.thu_nhap
     def nhap_nam(self):
         print("Nhập những năm bạn muốn tính thuế ")
         self.nam = [int(x) for x in input().split()]
 
     def tinh_thue(self):
-        self.thu_nhap_chiu_thue = int(self.thu_nhap) - int(self.tien_bao_hiem) - int(self.mien_tru) - int(self.so_nguoi_phu_thuoc) * 4400000
+        self.thu_nhap_chiu_thue = int(self.thu_nhap) - int(self.tien_bao_hiem) - int(self.mien_tru_thang) - int(
+            self.so_nguoi_phu_thuoc) * 4400000
+        self.thu_nhap_nam += self.thu_nhap
         if self.thu_nhap_chiu_thue < 0:
             self.tien_thue_thang = 0
             self.thu_nhap_chiu_thue = 0
@@ -43,14 +48,37 @@ class TinhThue:
         elif 10000000 < self.thu_nhap_chiu_thue <= 18000000:
             self.tien_thue_thang = int(0.05 * 5000000 + 0.1 * 5000000 + (self.thu_nhap_chiu_thue - 10000000) * 0.15)
         elif 18000000 < self.thu_nhap_chiu_thue <= 32000000:
-            self.tien_thue_thang = int(0.05 * 5000000 + 0.1 * 5000000 + 0.15 * 8000000 + (self.thu_nhap_chiu_thue - 18000000) * 0.2)
+            self.tien_thue_thang = int(
+                0.05 * 5000000 + 0.1 * 5000000 + 0.15 * 8000000 + (self.thu_nhap_chiu_thue - 18000000) * 0.2)
         elif 32000000 < self.thu_nhap_chiu_thue <= 52000000:
             self.tien_thue_thang = int(0.05 * 5000000 + 0.1 * 5000000 + 0.15 * 8000000 + 0.2 * 14000000 + (
                     self.thu_nhap_chiu_thue - 32000000) * 0.25)
         elif 52000000 < self.thu_nhap_chiu_thue <= 80000000:
-            self.tien_thue_thang = int(0.05 * 5000000 + 0.1 * 5000000 + 0.15 * 8000000 + 0.2 * 14000000 + 0.25 * 20000000 + (
-                    self.thu_nhap_chiu_thue - 52000000) * 0.3)
+            self.tien_thue_thang = int(
+                0.05 * 5000000 + 0.1 * 5000000 + 0.15 * 8000000 + 0.2 * 14000000 + 0.25 * 20000000 + (
+                        self.thu_nhap_chiu_thue - 52000000) * 0.3)
         elif self.thu_nhap_chiu_thue > 80000000:
-            self.tien_thue_thang = int(0.05 * 5000000 + 0.1 * 5000000 + 0.15 * 8000000 + 0.2 * 14000000 + 0.25 * 20000000 + 0.3 * 28000000 + (
-                    self.thu_nhap_chiu_thue - 80000000) * 0.35)
+            self.tien_thue_thang = int(
+                0.05 * 5000000 + 0.1 * 5000000 + 0.15 * 8000000 + 0.2 * 14000000 + 0.25 * 20000000 + 0.3 * 28000000 + (
+                        self.thu_nhap_chiu_thue - 80000000) * 0.35)
+
+    def tinh_thue_nam(self):
+        self.thu_nhap_chiu_thue_nam = self.thu_nhap_nam - self.tien_bao_hiem - self.mien_tru_nam - self.so_nguoi_phu_thuoc * 4400000 * 12
+        if self.thu_nhap_chiu_thue_nam < 0:
+            self.thu_nhap_chiu_thue_nam = 0
+            self.tien_thue_thuc_te = 0
+        if 0 < self.thu_nhap_chiu_thue_nam <= 60000000:
+            self.tien_thue_thuc_te = int(self.thu_nhap_chiu_thue_nam * 0.05)
+        elif 60000000 < self.thu_nhap_chiu_thue_nam <= 120000000:
+            self.tien_thue_thuc_te = int(self.thu_nhap_chiu_thue_nam * 0.1)
+        elif 120000000 < self.thu_nhap_chiu_thue_nam <= 210000000:
+            self.tien_thue_thuc_te = int(self.thu_nhap_chiu_thue_nam * 0.15)
+        elif 210000000 < self.thu_nhap_chiu_thue_nam <= 384000000:
+            self.tien_thue_thuc_te = int(self.thu_nhap_chiu_thue_nam * 0.2)
+        elif 384000000 < self.thu_nhap_chiu_thue_nam <= 624000000:
+            self.tien_thue_thuc_te = int(self.thu_nhap_chiu_thue_nam * 0.25)
+        elif 624000000 < self.thu_nhap_chiu_thue_nam <= 960000000:
+            self.tien_thue_thuc_te = int(self.thu_nhap_chiu_thue_nam * 0.3)
+        elif self.thu_nhap_chiu_thue_nam > 960000000:
+            self.tien_thue_thuc_te = int(self.thu_nhap_chiu_thue_nam * 0.35)
 
